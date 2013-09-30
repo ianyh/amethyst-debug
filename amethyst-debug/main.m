c42b3361dd4b484ef3293b7917316bf1c00aa862
Ian Ynda-Hummel <ianynda@gmail.com>
2013-09-29 21:20:44 -0400

    Initial Commit

---
+++ amethyst-debug/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  amethyst-debug
+//
+//  Created by Ian Ynda-Hummel on 9/29/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
